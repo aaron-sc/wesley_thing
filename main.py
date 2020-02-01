1308416360bd875e4283a409e5433f09944d47ef
Aaron Santa Cruz <aaron.santacruz03@gmail.com>
2020-02-01 20:22:07 +0000

    Made it better

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import os
 # below is if you need to be promted each time
 # dest = (input("Destination: ")
+def copy(starting_dest, ending_dest):
+	# Copy
+	command = os.popen('cp ' + starting_dest + '*' + ending_dest)
+	print(command.read())
+	print(command.close())
 
-# Copy
-dest = "/path/to/file/"
-command = os.popen('cp /path/to/file/* ' + str(dest))
-print(command.read())
-print(command.close())
-
-# Delete
-command = os.popen('rm -r '+ str(dest))
-print(command.read())
-print(command.close())
-
-print("Complete!")
+	# Delete
+	command = os.popen('rm -r ' + ending_dest + '*')
+	print(command.read())
+	print(command.close())
+	print("Complete!")
